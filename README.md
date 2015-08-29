e6370767f8750ec5390b672112edebbbb2e1e5c3
malnick <malnick@gmail..com>
2015-08-29 11:00:28 -0700

    it begins

---
+++ README.md
@@ -0,0 +1,69 @@
+# REST HaProxy
+REST HaProxy exposes the server inputs in haproxy.cfg as JSON parameters so you can query any haproxy and know what services are available, on what ports.
+
+Defaults expose on :3000 @ /services
+
+We use this for exposing haproxy services to query internal /info endpoints that we can use to aggregate known running versions of microservices within our infrastrcture. 
+
+## Design
+Given a haproxy configuration at path ```/etc/haproxy/haproxy.cfg```:
+
+```ini
+global
+  daemon
+  group  haproxy
+  log  127.0.0.1 local0
+  log  127.0.0.1 local1 notice
+  maxconn  4096
+
+defaults
+  log global
+  mode  http
+
+frontend http-in
+  bind *:80
+  acl service path_beg -t /service
+  acl other_service path_beg -t /other_service
+  default backend my_service
+  use_backend service if my_service
+  use_backend other_service if other_service
+
+backend service
+  balance leastconn
+  server service-01 10.0.1.10:31501 check port 32501
+  server service-02 10.0.1.10:21502 check port 32502
+  server service-03 10.0.2.10:31500 check port 32501
+  server service-04 10.0.2.11:31500 check port 32502
+
+backend other_service
+  balance leastconn
+  server service-01 10.0.5.10:31501 check 
+  server service-02 10.0.5.10:21502 check
+  server service-03 10.0.5.10:31503 check 
+  server service-04 10.0.5.11:31500 check
+```
+
+Will result in the following JSON endpoint available at: ```localhost:3000/services```
+
+map[string]map[string][]string
+
+```json
+{
+  "Service": {
+    "other_service": [
+      "10.0.5.10:31501",
+      "10.0.5.10:21502",
+      "10.0.5.10:31503",
+      "10.0.5.11:31500"
+    ],
+    "service": [
+      "10.0.1.10:31501 10.0.1.10:32501",
+      "10.0.1.10:21502 10.0.1.10:32502",
+      "10.0.2.10:31500 10.0.2.10:32501",
+      "10.0.2.11:31500 10.0.2.11:32502"
+    ]
+  }
+}
+```
+
+The second set of IP's in 'service' Service is the address to ```check port $someport```, which is usually a management address.
---
+++ rest_haproxy.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Services struct {
+	Service map[string][]string
+}
+
+type Store struct {
+	Name string
+}
+
+// Store the sub backend here in the sub for loop of the file parser
+func (s Store) storedBackend() string {
+	return s.Name
+}
+
+func getBackend(line string) (backend string, err error) {
+
+	// Regex for Backend
+	match_bkend, err := regexp.Compile(`^\s*backend`)
+	if err != nil {
+		return "Failed matching backend", err
+	}
+	if match_bkend.MatchString(line) {
+		larry := strings.Fields(line)
+		name := larry[1]
+		backend = name
+		log.Println("MATCHED BACKEND: ", backend)
+		return backend, nil
+	}
+
+	return "null", nil
+}
+
+func getIp(line string) (ip string, err error) {
+	// Regex for Server
+	match_srv, err := regexp.Compile(`^\s*server`)
+	if err != nil {
+		return "Failed.", err // there was a problem with the regular expression.
+	}
+
+	if match_srv.MatchString(line) {
+		larry := strings.Fields(line)
+		svc_address := larry[2]
+		// Get mgmt address
+		if len(larry) > 5 {
+			mgmt_port := larry[len(larry)-1]
+			dest := strings.Split(svc_address, ":")
+			ip := dest[0]
+			log.Println("IP: ", ip)
+			log.Println("MGMT: ", mgmt_port)
+			mgmt_temp := []string{ip, ":", mgmt_port}
+			mgmt_address := strings.Join(mgmt_temp, "")
+			final_temp := []string{svc_address, mgmt_address}
+			final := strings.Join(final_temp, " ")
+			log.Println("MATCHED MGMT SERVER: ", final)
+			return final, nil
+		}
+		log.Println("MATCHED SERVER: ", svc_address)
+		return svc_address, nil
+	}
+	return "null", nil
+}
+
+func parsefile(filename string) (s Services, err error) {
+	// You know, a simple []map[string]map[string][]string
+	s.Service = make(map[string][]string)
+	// Stupid hack that could probably be solved with recursion...
+	var store Store
+
+	// Handle the file
+	inFile, _ := os.Open(filename)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	// Two nested loops to deal with scoping of next backend being the last arg in the nested loop
+	for scanner.Scan() {
+		line := scanner.Text()
+		log.Println("STORED BACKEND: ", store.Name)
+		if len(store.Name) == 0 {
+			backend_name, _ := getBackend(line)
+			if backend_name != "null" {
+				s.Service[backend_name] = []string{}
+				for scanner.Scan() {
+					line := scanner.Text()
+					check, _ := getBackend(line)
+					if check != "null" {
+						store.Name = check
+						break
+					}
+					server_ip, _ := getIp(line)
+					if server_ip != "null" {
+						s.Service[backend_name] = append(s.Service[backend_name], server_ip)
+					}
+				}
+			}
+		} else {
+			backend_name := store.Name
+			if backend_name != "null" {
+				s.Service[backend_name] = []string{}
+				for scanner.Scan() {
+					line := scanner.Text()
+					check, _ := getBackend(line)
+					if check != "null" {
+						store.Name = check
+						break
+					}
+					server_ip, _ := getIp(line)
+					if server_ip != "null" {
+						s.Service[backend_name] = append(s.Service[backend_name], server_ip)
+					}
+				}
+			}
+		}
+	}
+
+	log.Println("Final Hash:\n")
+	log.Println(s)
+	return s, nil
+}
+
+func response(rw http.ResponseWriter, request *http.Request) {
+	services, err := parsefile("/etc/haproxy/haproxy.cfg")
+	if err != nil {
+		log.Println("ERROR: ", err)
+	}
+	json, err := json.Marshal(services)
+	rw.Write([]byte(json))
+}
+
+func main() {
+	http.HandleFunc("/services", response)
+	http.ListenAndServe(":3000", nil)
+}
